894c0a4cf166d4d807bfc625b1c705a2f1e3b109
Nathan Steed <steedn@outlook.com>
2021-07-06 15:32:10 +0000

    Automatically backed up by Learn

---
+++ class.rb
@@ -0,0 +1,29 @@
+class Song 
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM songs 
+    SQL
+    
+    DB[:conn].execute(sql).map do |row|
+    self.new_from_db(row)
+    end
+  end
+  
+  def self.new_from_db(row) 
+    new_song = self.new 
+    new_song.id = row[0]
+    new_song.name = row[1]
+    new_song.length = row[2]
+    puts new_song
+  end
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM songs
+    WHERE name = ?
+    LIMIT 1 
+    SQL
+    DB[:conn].execute(sql, name).map do |row| 
+      self.new_from_db(row)
+    end.first 
+  end
+end
